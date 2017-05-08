ce1ffc9a3ced6939053b48b901c53dd22bdb7c66
= <asen.nikolaev95@abv.bg>
2017-05-08 13:52:12 +0300

    adding documentations

--- src/HTMLFileManagement.java
+++ src/HTMLFileManagement.java
@@ -6,12 +6,31 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.Random;
 
+/**
+ * 
+ * HTMLFileManagement has one static method upload - which every second get's
+ * çalled. We open BufferWriter to the html file and write the new content every
+ * second. The html file has this tag - <meta http-equiv=\"refresh\"
+ * content=\"1\"/>\n" - which ensures that the browser will call the page every
+ * second. Before we upload html file,first we upload css file. After the body
+ * tag - we called randomBreakLines() method which returns a random string of
+ * <br>
+ * tags to ensure random position of the text.
+ * 
+ * @author asen
+ *
+ */
 public class HTMLFileManagement {
 
 	private static final String HTML_FILE_PATH = "online_timer.html";
 
 	private static final StyleManagement css = new StyleManagement();
 
+	/**
+	 * Upload the content every second
+	 * @param date current date
+	 * @param time current time
+	 */
 	public static void upload(LocalDate date, LocalTime time) {
 
 		css.upload();
@@ -24,11 +43,10 @@ public class HTMLFileManagement {
 			writer.write("<title>Online Clock</title>\n");
 			writer.write("<meta http-equiv=\"refresh\" content=\"1\"/>\n");
 			writer.write("<link rel=\"stylesheet\" type=\"text/css\" href=\"stylesheet.css\">");
-		
-		
+
 			writer.write("</head>\n");
 			writer.write("<body>\n");
-		
+
 			writer.write(randomBreakLines());
 			writer.write("<h1><p align=\"center\">" + date + " " + time + "</p></h1>\n");
 
@@ -53,10 +71,14 @@ public class HTMLFileManagement {
 		System.out.println("UPLOAD HTML");
 	}
 
+	/**
+	 * 
+	 * @return random numbers of <br> tags as String
+	 */
 	private static String randomBreakLines() {
 		StringBuilder builder = new StringBuilder();
 		Random random = new Random();
-		
+
 		int count = random.nextInt(15);
 		for (int i = 0; i < count; i++) {
 			builder.append("<br> </br> \n");
